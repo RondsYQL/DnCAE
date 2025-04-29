df00d7a40ea753fed952ee437993b8d45d3137a5
lyqnznb <2633692290@qq.com>
2025-04-29 12:28:28 +0800

    fifth change

--- DnCAE.py
+++ DnCAE.py
@@ -2,7 +2,7 @@ from collections import OrderedDict
 import torch.nn as nn
 import torch
 
-# DnCAE的模型结构
+# DnCAE模型的结构
 # picture_channel代表图像通道数N，channels代表卷积层通道数C
 picture_channel = 3
 channels = 64
